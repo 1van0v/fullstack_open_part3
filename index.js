dd5eac40192b6922c9aa0e66cd58c8fdcb30533d
Sashka <ivanov5oleksandr@gmail.com>
2020-07-05 18:09:53 +0300

    bind to port defined in env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let { persons } = require('./persons');
 const getId = require('./idGenerator');
 const logger = require('./logger');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
